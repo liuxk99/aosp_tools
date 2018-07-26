2cd59861130b19e979e776cd2b0ad99922474249
Liu XianKe <liuxianke@letv.com>
2018-07-26 10:25:38 +0800

    <improvement>: avoid installed dir disturbution.

--- sync_build.sh
+++ sync_build.sh
@@ -7,5 +7,8 @@ repo forall -c 'git reset --hard; git clean -fd'
 # sync lastest code
 repo sync --no-tags -cdj4
 
+# rm installed dirs
+rm $OUT/system $OUT/data $OUT/rootdir -rf
+
 # make
 time make -j16
