cf694a6303c907abe247a71019fa340c249bd5a3
Liu XianKe <liuxianke@letv.com>
2018-09-05 18:48:05 +0800

    <improvement>: use index of java version to avoid interacting.

--- switch_java_ver.sh
+++ switch_java_ver.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 # must run as root(sudo)
 
-update-alternatives --config java
-update-alternatives --config javac
-update-alternatives --config javap
-update-alternatives --config javaws
-update-alternatives --config javadoc
-update-alternatives --config javah
+idx=$1
+
+update-alternatives --config java <<EOF
+$idx
+EOF
+
+update-alternatives --config javac <<EOF
+$idx
+EOF
+
+update-alternatives --config javap <<EOF
+$idx
+EOF
+
+update-alternatives --config javaws <<EOF
+$idx
+EOF
+
+update-alternatives --config javadoc <<EOF
+$idx
+EOF
+
+update-alternatives --config javah <<EOF
+$idx
+EOF
