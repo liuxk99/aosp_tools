918720adf5eaa910409c0e2270885b9e0453e693
Liu XianKe <liuxianke@le.com>
2019-04-23 22:45:08 +0800

    <feature>: build scripts of mstar848 for crontab.

---
+++ mstar848_clean_build-userdebug.sh
@@ -0,0 +1,33 @@
+# !/bin/bash
+
+DIR=/home/thomas/eui/mstar848
+if [ ! -z $DIR ]; then
+  # rm installed dirs
+  # echo $DIR 'exist'
+
+  cd $DIR
+  if [ ! -z .repo ]; then
+    # echo '.repo exist'
+    rm .repo -rf
+    rm * -rf
+  else
+    exit 1
+  fi
+
+else
+  echo '$DIR is empty!'
+  exit 2
+fi
+
+repo init -u athenar:mstar/6A848/platform/manifest.git -b master -m letv/JASON.xml --repo-url=athenar:tools/repo.git --no-repo-verify
+repo sync --no-tags -cdj4
+
+cd $DIR
+source build/envsetup.sh
+lunch <<EOF
+cibn
+letv_jason-userdebug
+EOF
+
+make -j16
+
