8e893ad9d4daa807769f8ac6ca2bbae6cd2320b7
Liu XianKe <liuxianke@le.com>
2019-06-24 16:54:19 +0800

    <feature>: build scripts for mstar648.

---
+++ mstar648_build-x450uc-user.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+
+#ROOT=/disk2/thomas/eui/mstar846
+
+#cd $ROOT
+.repo/repo/repo forall -c 'git reset --hard; git clean -fd'
+.repo/repo/repo sync --no-tags -cj4
+
+export USER=thomas
+
+source build/envsetup.sh
+lunch <<EOF
+cibn
+aosp_almond_letv_hera_dtmb_x450uc-user
+EOF
+
+# cd $ROOT
+# if [ -d "$ROOT/out" ]; then
+#     echo "rm -rf $ROOT/out"
+#     rm -rf $ROOT/out
+# else
+#     echo "no out dir!"
+# fi
+
+time make -j4
