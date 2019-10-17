87670ff54df7fdfca42a6bf256eafa08e41f8daa
Liu XianKe <liuxianke@le.com>
2019-10-17 14:05:15 +0800

    <M>: add mstar938, mstar358 and mstar848 for schedule.

--- repo-mirror-schedule-sync.sh
+++ repo-mirror-schedule-sync.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-HOME=/disk2/thomas/mirror/mstar648
+mstar938=/disk2/thomas/mirror/mstar938
+mstar648=/disk2/thomas/mirror/mstar648
+mstar358=/disk2/thomas/mirror/mstar358
+mstar848=/disk2/thomas/mirror/mstar848
 
-cd $HOME
+cd $mstar938
+repo sync -j4
+
+cd $mstar648
+repo sync -j4
+
+cd $mstar358
+repo sync -j4
+
+cd $mstar848
 repo sync -j4
