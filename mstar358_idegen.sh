43d696f59a9858273d416af6f8ef86b3e9e2a938
Thomas Liu <liuxk99@gmail.com>
2019-12-16 18:41:40 +0800

    <feature: DevEnv(mstar358)>·lunch.

--- mstar358_idegen.sh
+++ mstar358_idegen.sh
@@ -3,7 +3,7 @@
 source build/envsetup.sh
 lunch <<EOF
 cibn
-aosp_persimmon_isdb-userdebug
+aosp_persimmon_isdb_letv_y32s-userdebug
 EOF
 make out/host/linux-x86/framework/idegen.jar
 development/tools/idegen/idegen.sh
